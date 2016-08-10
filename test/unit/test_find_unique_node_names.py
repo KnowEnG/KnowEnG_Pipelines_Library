d4e167815f986dc2b7cb00e74cc4f1a78252f039
Suyang Chen <schen154@illinois.edu>
2016-08-10 14:42:58 -0500

    added test

--- test/unit/test_find_unique_node_names.py
+++ test/unit/test_find_unique_node_names.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
-
+import knpackage as kn
 
 class TestFind_unique_node_names(TestCase):
     def test_find_unique_node_names(self):
-        self.fail()
+        list_1 = [1,2,3,a,t]
+        list_2 = [a,3,4,b]
+        ret = find_unique_node_names(list_1, list_2)
+        self.assertEqual(True, set([3,a])==set(ret))
+    
