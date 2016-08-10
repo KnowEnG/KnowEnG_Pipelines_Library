680a7dace347f1f0f08a83c3eb4703069195b136
XICHEN24 <xichen24@illinois.edu>
2016-08-10 16:58:44 -0500

    add one test case
    add one test case to create_reverse_node_names_dict

--- test/unit/test_create_reverse_node_names_dict.py
+++ test/unit/test_create_reverse_node_names_dict.py
@@ -2,11 +2,14 @@ from unittest import TestCase
 import unittest
 import knpackage.toolbox as kn
 
-
 class TestCreate_reverse_node_names_dict(TestCase):
-    def test_create_reverse_node_names_dict(self):
-        res = kn.create_reverse_node_names_dict({'a': 0, 'b': 1})
-        compare_res = {0: 'a', 1: 'b'}
+    def test_dict_size_one(self):
+        res = kn.create_reverse_node_names_dict({'a': 0})
+        compare_res = {0: 'a'}
+        self.assertEqual(res, compare_res)
+    def test_dict_size_three_mix_type(self):
+        res = kn.create_reverse_node_names_dict({'a': 0, 1: 'b', 2: 8})
+        compare_res = {0: 'a', 'b': 1, 8: 2}
         self.assertEqual(res, compare_res)
 
 if __name__ == '__main__':
