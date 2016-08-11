97deb416827a2fa5dc05f7e95bfa087f5dd75be5
Suyang Chen <schen154@illinois.edu>
2016-08-11 17:17:17 -0500

    added test find dropped list

---
+++ test/unit/test_find_dropped_node_names.py
@@ -0,0 +1,7 @@
+import unittest
+from unittest import TestCase
+import knpackage.toolbox as kn
+import pandas as pd
+
+class TestFind_dropped_node_names(TestCase):
+    def test_find_dropped_node_names(self):
