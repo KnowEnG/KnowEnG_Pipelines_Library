20ef04f26b7f449e003fb9c4c0d9758f28a415ca
XICHEN24 <xichen24@illinois.edu>
2016-08-11 11:58:58 -0500

    combine map node to multiple
    put map node names to index to multiple functions

--- test/unit/test_multiple_functions.py
+++ test/unit/test_multiple_functions.py
@@ -38,6 +38,23 @@ class TestMultiple_functions(TestCase):
         compare_result.index = ['a', 'b', 'c']
         self.assertEqual(True, compare_result.equals(res))
 
+    def test_map_node_names_to_index_node_1(self):
+        network = pd.DataFrame([['a', 'b', 1], ['c', 'd', 1]])
+        network.columns = ['node_1', 'node_2', 'wt']
+        genes_map = {'a': 0, 'b': 1, 'c':2, 'd':3}
+        res = kn.map_node_names_to_index(network, genes_map, 'node_1')
+        compare_result = pd.DataFrame([[0, 'b', 1], [2, 'd', 1]])
+        compare_result.columns = ['node_1', 'node_2', 'wt']
+        self.assertEqual(True, compare_result.equals(res))
+
+    def test_map_node_names_to_index_node_2(self):
+        network = pd.DataFrame([['a', 'b', 1], ['c', 'd', 1]])
+        network.columns = ['node_1', 'node_2', 'wt']
+        genes_map = {'a': 0, 'b': 1, 'c': 2, 'd': 3}
+        res = kn.map_node_names_to_index(network, genes_map, 'node_2')
+        compare_result = pd.DataFrame([['a', 1, 1], ['c', 3, 1]])
+        compare_result.columns = ['node_1', 'node_2', 'wt']
+        self.assertEqual(True, compare_result.equals(res))
 
 if __name__ == '__main__':
     unittest.main()
