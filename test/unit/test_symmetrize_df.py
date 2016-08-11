c03e9624062db305314b79c706825c2991f0462c
XICHEN24 <xichen24@illinois.edu>
2016-08-11 10:39:42 -0500

    test for symmetrize dataframe

---
+++ test/unit/test_symmetrize_df.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest import TestCase
+import knpackage.toolbox as kn
+import pandas as pd
+
+class TestSymmetrize_df(TestCase):
+    def test_symmetrize_df(self):
+        res = pd.DataFrame([[1, 2, 1], [3, 4, 1], [2, 1, 1], [4, 3, 1]])
+        res.columns = ['node_1', 'node_2', 'wt']
+        network = pd.DataFrame([[1, 2, 1], [3, 4, 1]])
+        network.columns = ['node_1', 'node_2', 'wt']
+        compare_result = kn.symmetrize_df(network)
+        print(compare_result)
+        print(res)
+        self.assertEqual(True, compare_result.equals(res))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
