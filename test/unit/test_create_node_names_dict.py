6e09019b670cf6de860e982f8676d85b51e572c8
XICHEN24 <xichen24@illinois.edu>
2016-08-11 23:46:44 -0500

    change unit test
    change unit test for create_node_names_dict

--- test/unit/test_create_node_names_dict.py
+++ test/unit/test_create_node_names_dict.py
@@ -4,27 +4,35 @@ import knpackage.toolbox as kn
 
 
 class TestCreate_node_names_dict(TestCase):
-    def test_string_keys(self):
-        node_names = ['a', 'b', 'c']
-        res = kn.create_node_names_dict(node_names, start_value=0)
-        compare_res = {'a': 0, 'b': 1, 'c': 2}
-        self.assertEqual(res, compare_res)
-    def test_int_keys(self):
-        node_names = [4, 5, 6]
-        res = kn.create_node_names_dict(node_names, start_value=0)
-        compare_res = {4: 0, 5: 1, 6: 2}
-        self.assertEqual(res, compare_res)
-    def test_mix_keys_start_nonzero(self):
-        node_names = [4, "test1", 6, "test2"]
-        res = kn.create_node_names_dict(node_names, start_value=8)
-        compare_res = {4: 8, "test1": 9, 6: 10, "test2":11}
-        self.assertEqual(res, compare_res)
+    def setUp(self):
+        self.node_names = ['a', 'b', 'c']
+        self.node_names_empty = []
+        self.node_names_start_positive = ['a', 8, 'b']
+        self.node_names_start_negative = ['a', 8, 'b']
 
-    def test_empty_dict(self):
-        node_names = []
-        res = kn.create_node_names_dict(node_names, start_value=0)
-        compare_res = {}
-        self.assertEqual(res, compare_res)
+    def tearDown(self):
+        del self.node_names
+        del self.node_names_empty
+        del self.node_names_start_positive
+        del self.node_names_start_negative
+
+    def test_create_node_names_dict(self):
+        ret = kn.create_node_names_dict(self.node_names, start_value=0)
+        self.assertEqual(ret, {'a': 0, 'b': 1, 'c': 2}, 'wrong output')
+
+    def test_create_node_names_dict_empty(self):
+        ret = kn.create_node_names_dict(self.node_names_empty, start_value=0)
+        self.assertEqual(ret, {}, 'wrong output for empty node_names')
+
+    def test_create_node_names_dict_start_positive(self):
+        ret = kn.create_node_names_dict(self.node_names_start_positive, start_value=5)
+        self.assertEqual(ret, {'a': 5, 8: 6, 'b':7},
+                         'wrong output for node_names with positive start value')
+
+    def test_create_node_names_dict_start_negative(self):
+        ret = kn.create_node_names_dict(self.node_names_start_negative, start_value=-3)
+        self.assertEqual(ret, {'a': -3, 8: -2, 'b': -1},
+                         'wrong output for node_names with negative start value')
 
 if __name__ == '__main__':
     unittest.main()
