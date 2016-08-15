58787e9770792df50508412c989612642b0938b9
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2016-08-15 13:54:26 -0500

    -m "run file to perform all tests in test_clustering_functions.py

--- test/unit/test_clustering_functions_runner.py
+++ test/unit/test_clustering_functions_runner.py
@@ -17,12 +17,12 @@ sys.path.extend(sample_clustering_unit_test_directory)
 import test_clustering_functions as tkeg
 
 #                               method A
-test_suite = unittest.TestSuite()
-test_suite.addTest(unittest.makeSuite(tkeg.test_clustering_functions))
-myrunner = unittest.TextTestRunner()
-myResult = myrunner.run(test_suite)
+#test_suite = unittest.TestSuite()
+#test_suite.addTest(unittest.makeSuite(tkeg.test_clustering_functions))
+#myrunner = unittest.TextTestRunner()
+#myResult = myrunner.run(test_suite)
 
 #                               method B
-# mySuit = tkeg.suite()
-# runner = unittest.TextTestRunner()
-# myResult2 = runner.run(mySuit)
+mySuit = tkeg.suite()
+runner = unittest.TextTestRunner()
+myResult2 = runner.run(mySuit)
