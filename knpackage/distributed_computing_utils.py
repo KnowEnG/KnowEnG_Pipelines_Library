d256d465692abc884c3af13a08ba94bfe4ce5c5b
candicegjing <candicegjing@gmail.com>
2017-01-25 14:39:17 -0600

    changing debugging level for dispy

--- knpackage/distributed_computing_utils.py
+++ knpackage/distributed_computing_utils.py
@@ -18,12 +18,11 @@ def generate_compute_clusters(cluster_ip_addresses, func_name, dependency_list):
     '''
     import sys
     import dispy
-    import logging
 
     try:
         cluster_list = []
         range_list = range(0, len(cluster_ip_addresses))
-        print(range_list)
+
         for i in range_list:
             cur_cluster = dispy.JobCluster(func_name,
                                            nodes=[cluster_ip_addresses[i]],
@@ -53,7 +52,7 @@ def create_cluster_worker(cluster, i, *args_to_func):
 
     print("Start creating clusters {}.....".format(str(i)))
     try:
-        print("len of send_args = {}".format(len(args_to_func)))
+        print("Length of passing arguments = {}".format(len(args_to_func)))
         job = cluster.submit(*args_to_func)
         job.id = i
         ret = job()
