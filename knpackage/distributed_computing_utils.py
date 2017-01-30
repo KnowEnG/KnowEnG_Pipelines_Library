5aaed7c26b76593442a8346969ef8368fc40ff15
candicegjing <candicegjing@gmail.com>
2017-01-30 16:57:52 -0600

    Fix bugs

--- knpackage/distributed_computing_utils.py
+++ knpackage/distributed_computing_utils.py
@@ -169,7 +169,7 @@ def determine_parallelism_locally(number_of_loops):
     import multiprocessing
 
     number_of_cpu = multiprocessing.cpu_count()
-    if (number_of_cpu == 0 or number_of_loops == 0):
+    if (number_of_loops == 0):
         return 1;
     if (number_of_loops < number_of_cpu):
         return number_of_loops
